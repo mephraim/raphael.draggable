6efb85d0667f4cbf3308ec21fa04fb71269e022c
Matthew Ephraim <mephraim@matthew-ephraims-computer.local>
2009-10-22 00:19:15 -0500

    tested and implemented basic behavior for the draggable plugin

--- spec/raphael.draggable_spec.js
+++ spec/raphael.draggable_spec.js
@@ -5,62 +5,66 @@ Screw.Unit(function() {
       paper = Raphael(0,0, 600, 600);
     });
     
-    it("should add a draggable function to the raphael paper ", function() {
-      expect(jQuery.isFunction(paper.draggable)).to(equal, true);
+    it("adds a draggable namespace to the raphael paper", function() {
+      expect(paper.draggable).to_not(equal, null);
+    });
+    
+    it("add sa draggable.enable function to the raphael paper", function() {
+      expect(jQuery.isFunction(paper.draggable.enable)).to(equal, true);
     });  
     
-    describe("The draggable initialization function for the paper", function() {
+    describe("The draggable.enable function for the paper", function() {
       var paper;
       before(function() {
         paper = Raphael(0,0, 600, 600);
-        paper.draggable();
+        paper.draggable.enable();
       });
-      
-      it("should return the paper object after being called", function() {
+            
+      it("returns the paper object after being called", function() {
         var paper = Raphael(0, 0, 600, 600);
-        expect(paper.draggable()).to(equal, paper);
+        expect(paper.draggable.enable()).to(equal, paper);
       });
       
-      it("should set a new property isDraggable for the paper to true", function() {
-        expect(paper.isDraggable).to(equal, true);
+      it("sets a new property draggable.enabled for the paper to true", function() {
+        expect(paper.draggable.enabled).to(equal, true);
       });
       
-      it("should add a setCurrentDraggable function to the paper ", function() {
-        expect(jQuery.isFunction(paper.setCurrentDraggable)).to(equal, true);
+      it("adds a draggable.current function to the paper ", function() {
+        expect(jQuery.isFunction(paper.draggable.current)).to(equal, true);
       });
       
-      it("should add a getCurrentDraggable function to the paper", function() {
-        expect(jQuery.isFunction(paper.getCurrentDraggable)).to(equal, true);
+      it("adds a draggable.clearCurrent function to the paper", function() {
+        expect(jQuery.isFunction(paper.draggable.clearCurrent)).to(equal, true);
       });
-      
-      it("should add a clearCurrentDraggable function to the paper", function() {
-        expect(jQuery.isFunction(paper.clearCurrentDraggable)).to(equal, true);
-      });
-      
-      it("should store the current draggable", function() {
-        var element = "test";
-        paper.setCurrentDraggable(element);
-        expect(paper.getCurrentDraggable()).to(equal, element);
-      });
-      
-      it("set the current draggable to null when clearCurrentDraggable is called", function() {
-        paper.setCurrentDraggable("test");
-        paper.clearCurrentDraggable();
-        expect(paper.getCurrentDraggable()).to(equal, null);
+            
+      it("sets the current draggable to null when draggable.clearCurrent is called", function() {
+        paper.draggable.current("test");
+        paper.draggable.clearCurrent();
+        expect(paper.draggable.current()).to(equal, null);
       });
     });
   
-    describe("The setCurrentDraggable function", function() {
-      it("should return the paper after being called", function() {
-        paper.draggable();
-        expect(paper.setCurrentDraggable("test")).to(equal, paper);
+    describe("The draggable.current function", function() {
+      before(function() {
+        paper = Raphael(0, 0, 600, 600);
+        paper.draggable.enable();
+      });
+      
+      it("returns the paper after being called", function() {
+        expect(paper.draggable.current("test")).to(equal, paper);
+      });
+      
+      it("stores the current draggable", function() {
+        var element = "test";
+        paper.draggable.current(element);
+        expect(paper.draggable.current()).to(equal, element);
       });
     });
     
     describe("The clearCurrentDraggable function", function() {
-      it("should return the paper after being called", function() {
-        paper.draggable();
-        expect(paper.clearCurrentDraggable()).to(equal, paper);
+      it("returns the paper after being called", function() {
+        paper.draggable.enable();
+        expect(paper.draggable.clearCurrent()).to(equal, paper);
       });
     });
   });
@@ -68,54 +72,45 @@ Screw.Unit(function() {
   describe("Draggable plugin for Raphael elements", function() {
     var rect;
     before(function() {
-      rect = Raphael(0, 0, 600, 600).rect(1,1,1,1);
-      rect.draggable();
+      rect = Raphael(0, 0, 600, 600).draggable.enable().rect(1,1,1,1);
+      rect.draggable.enable();
     });
     
-    it("should add a draggable function to raphael elements", function() {
-       expect(jQuery.isFunction(rect.draggable)).to(equal, true);
+    it("adds a draggable namespace to raphael elements", function() {
+      expect(rect.draggable).to_not(equal, null);
     });
     
-    describe("The draggable initialization function for elements", function() {
-      it("should return the element after being called", function() {
-        var rect = Raphael(0, 0, 600, 600).rect(1,1,1,1);
-        expect(rect.draggable()).to(equal, rect);
+    it("adds a draggable.enable function to raphael elements", function() {
+      expect(jQuery.isFunction(rect.draggable.enable)).to(equal, true);
+    });
+    
+    describe("The draggable.enable function for elements", function() {
+      it("returns the element after being called", function() {
+        expect(rect.draggable.enable()).to(equal, rect);
       });
+    });
+    
+    describe("the draggable.enable function", function() {
+      it("sets draggable.enabled to true when is called", function() {
+        rect.draggable.enabled = false;
+        rect.draggable.enable();
+        expect(rect.draggable.enabled).to(equal, true);
+      });
+      
+      it("returns the element after being called", function() {
+        expect(rect.draggable.enable()).to(equal, rect);
+      });
+    });
 
-      it("should set a new property isDraggable to true after being called", function() {
-        expect(rect.isDraggable).to(equal, true);
+    describe("the draggable.disable function", function() {
+      it("sets draggable.enabled to false when called", function() {
+        rect.draggable.enabled = true;
+        rect.draggable.disable();
+        expect(rect.draggable.enabled).to(equal, false);
       });
       
-      it("should add an enableDragging function to raphael elements", function() {
-        expect(jQuery.isFunction(rect.enableDragging)).to(equal, true);
-      });
-         
-      it("should add a disableDragging function to raphael elements", function() {
-        expect(jQuery.isFunction(rect.disableDragging)).to(equal, true);
-      });
-    });
-    
-    describe("the enableDragging function", function() {
-      it("should set isDraggable to true when enableDragging is called", function() {
-        rect.isDraggable = false;
-        rect.enableDragging();
-        expect(rect.isDraggable).to(equal, true);
-      });
-      
-      it("should return the element after being called", function() {
-        expect(rect.enableDragging()).to(equal, rect);
-      });
-    });
-    
-    describe("the disableDragging function", function() {
-      it("should set isDraggable to false when disableDragging is called", function() {
-        rect.isDraggable = true;
-        rect.disableDragging();
-        expect(rect.isDraggable).to(equal, false);
-      });
-      
-      it("should return the element after being called", function() {
-        expect(rect.disableDragging()).to(equal, rect);
+      it("returns the element after being called", function() {
+        expect(rect.draggable.disable()).to(equal, rect);
       });
     });
   });
